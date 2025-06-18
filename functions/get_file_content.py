e38ba77d4b9b68133be759ceb1fdbf4dd711e6e6
Drew Elliott <drew@beaverlab.dev>
2025-06-18 19:16:50 -0400

    minor fixes

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,34 +2,40 @@ import os
 from google.genai import types
 from config import MAX_CHARS
 
-def get_file_content(working_directory, file_path):
-    working_directory = os.path.abspath(working_directory)
-    combined_path = os.path.abspath(os.path.join(working_directory, file_path))
 
-    if not combined_path.startswith(working_directory):
+def get_file_content(working_directory, file_path):
+    abs_working_dir = os.path.abspath(working_directory)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
 
-    if not os.path.isfile(combined_path):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
-        with open(combined_path, "r") as fh:
-            file_content_string = fh.read(MAX_CHARS)
-        return file_content_string
+        with open(abs_file_path, "r") as fh:
+            content = fh.read(MAX_CHARS)
+            if len(content) == MAX_CHARS:
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return content
     except Exception as e:
         error_string = str(e)
         return f'Error: {error_string}'
 
+
 schema_get_file_content = types.FunctionDeclaration(
     name="get_file_content",
-    description="Reads content from the specified file.",
+    description=f"Reads and returns the first {
+        MAX_CHARS} characters of the content from a specified file within the working directory",
     parameters=types.Schema(
         type=types.Type.OBJECT,
         properties={
             "file_path": types.Schema(
                 type=types.Type.STRING,
-                description="The file to read content from, relative to the working directory.",
+                description="The path to the file whose content should be read, relative to the working directory.",
             ),
         },
+        required=["file_path"],
     ),
 )
